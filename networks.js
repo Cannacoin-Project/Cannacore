64dcf980e3cb9f28d870ab63f030f7dd7be5e65f
Josh <Josh@cannacoin.cc>
2014-12-13 16:55:59 -0800

    Fixed proto and other details

--- networks.js
+++ networks.js
@@ -4,17 +4,21 @@ var hex = function(hex) {
   return new Buffer(hex, 'hex');
 };
 
+// Use this to get reverse genesis/merkle.
+//echo -n '203067d812254f72cdc20099b4a077a380597cf97638a88c4157620202b055ba' | dd conv=swab | rev
+
+
 exports.livenet = {
   name: 'livenet',
-  magic: hex('fcc1b7dc'),
+  magic: hex('c7c0fcd5'),
   addressVersion: 28,
   privKeyVersion: 156,
   P2SHVersion: 5,
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('F1B4CDF03C86099A0758F1C018D1A10BF05AFAB436C92B93B42BB88970DE9821'),
-    merkle_root: hex('04C3AE72B7E099C2A3FE2862AB6280B09C8B1E4CD217237EBB2D0CACC20AAA68'),
+    hash: hex('2198de7089b82bb4932bc936b4fa5af00ba1d118c0f158079a09863cf0cdb4f1'),
+    merkle_root: hex('68aa0ac2ac0c2dbb7e2317d24c1e8b9cb08062ab6228fea3c299e0b772aec304'),
     height: 0,
     nonce: 1120954,
     version: 1,
@@ -34,22 +38,22 @@ exports.mainnet = exports.livenet;
 exports.testnet = {
   name: 'testnet',
   magic: hex('fcc1b7dc'),
-  addressVersion: 0x1C,
-  privKeyVersion: 0x9C,
+  addressVersion: 111,
+  privKeyVersion: 239,
   P2SHVersion: 196,
   hkeyPublicVersion: 0x043587cf,
   hkeyPrivateVersion: 0x04358394,
   genesisBlock: {
-    hash: hex('F1B4CDF03C86099A0758F1C018D1A10BF05AFAB436C92B93B42BB88970DE9821'),
-    merkle_root: hex('04C3AE72B7E099C2A3FE2862AB6280B09C8B1E4CD217237EBB2D0CACC20AAA68'),
+    hash: hex('ba55b002026257418ca83876f97c5980a377a0b49900c2cd724f2512d8673020'),
+    merkle_root: hex('0b9fec20e5668d517876c19dcde24160165533daeeb5403d21b7d6c515b842540x'),
     height: 0,
-    nonce: 1120954,
+    nonce: 1223606,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1382342087,
+    timestamp: 1416351712,
     bits: 504365055,
   },
   dnsSeeds: [],
-  defaultClientPort: 55444,
-  lastPoWBlock: 150
+  defaultClientPort: 17143,
+  lastPoWBlock: 1300
 };
